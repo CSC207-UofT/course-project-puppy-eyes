ddd68d67713400aee10f10df4a180ff0133744f2
Andrew Qiu <andrew@gameofapps.org>
2021-10-13 21:29:49 -0400

    Remove User dependency in UserCreatorResponseModel

--- src/UserCreatorResponseModel.java
+++ src/UserCreatorResponseModel.java
@@ -1,12 +1,42 @@
 public class UserCreatorResponseModel {
-    private User user;
+    private boolean isSuccess;
+    private String firstName;
+    private String lastName;
+    private String homeAddress;
+    private String password;
+    private String email;
 
-    public UserCreatorResponseModel(User user){
-        this.user = user;
+    public UserCreatorResponseModel(boolean isSuccess, String firstName, String lastName,
+                                    String homeAddress, String password, String email) {
+        this.isSuccess = isSuccess;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.homeAddress = homeAddress;
+        this.password = password;
+        this.email = email;
     }
 
-    public User getUser(){
-        return this.user;
+    public boolean isSuccess() {
+        return isSuccess;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
