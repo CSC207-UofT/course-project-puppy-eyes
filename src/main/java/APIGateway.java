aa5ad79290ecd650ab34f077bb624c27f5fe3f4a
Andrew Qiu <andrewqiu9774@gmail.com>
2021-10-14 10:51:09 -0400

    Update APIGateway to use a generic type

--- src/main/java/APIGateway.java
+++ src/main/java/APIGateway.java
@@ -1,5 +1,4 @@
-public interface APIGateway {
+public interface APIGateway<RequestType>{
     /* Create a new user and return a response in the form of a JSON string.*/
-    String createUser(String firstName, String lastName,
-                                        String homeAddress, String password, String email);
+    String createUser(RequestType request);
 }
