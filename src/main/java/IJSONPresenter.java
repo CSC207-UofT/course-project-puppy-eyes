fd752d207ccdf2d85abde16a7cd97ef61b0dce85
Andrew Qiu <andrew@gameofapps.org>
2021-10-13 22:06:57 -0400

    Add JSONPresenter and IJSONPresenter.

---
+++ src/main/java/IJSONPresenter.java
@@ -0,0 +1,6 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public interface IJSONPresenter {
+    /* Transform a Java Object into a JSON string */
+    String toJSON(Object obj) throws JsonProcessingException;
+}
---
+++ src/main/java/JSONPresenter.java
@@ -0,0 +1,15 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSONPresenter implements IJSONPresenter{
+    private final ObjectMapper mapper;
+
+    public JSONPresenter() {
+        mapper = new ObjectMapper();
+    }
+
+    @Override
+    public String toJSON(Object obj) throws JsonProcessingException {
+        return mapper.writeValueAsString(obj);
+    }
+}
