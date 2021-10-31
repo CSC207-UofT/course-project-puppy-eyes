741f0ae81054601c0ea71238b673169640c799a2
Kenneth Tran <kennethtran77@gmail.com>
2021-10-31 18:43:22 -0400

    Implement SessionController

---
+++ src/main/java/server/controllers/SessionController.java
@@ -0,0 +1,23 @@
+package server.controllers;
+
+import server.use_cases.SessionTokenGeneratorInputBoundary;
+import server.use_cases.SessionTokenGeneratorRequestModel;
+import server.use_cases.SessionTokenGeneratorResponseModel;
+
+public class SessionController implements ISessionController {
+
+    public SessionTokenGeneratorInputBoundary sessionTokenGenerator;
+
+    public SessionController(SessionTokenGeneratorInputBoundary sessionTokenGenerator) {
+        this.sessionTokenGenerator = sessionTokenGenerator;
+    }
+
+    @Override
+    public String generateJwt(String email, String password) {
+        SessionTokenGeneratorRequestModel request = new SessionTokenGeneratorRequestModel(email, password);
+        SessionTokenGeneratorResponseModel response = sessionTokenGenerator.generateSessionToken(request);
+        return response.getJwt();
+    }
+
+
+}
