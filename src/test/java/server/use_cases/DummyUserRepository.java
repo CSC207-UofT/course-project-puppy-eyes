d6b51701c48d5f8a55b4ccd685d2208be6f9abba
Kenneth Tran <kennethtran77@gmail.com>
2021-11-12 05:27:03 -0500

    Change User to contain currentAddress and currentCity

--- src/test/java/server/use_cases/DummyUserRepository.java
+++ src/test/java/server/use_cases/DummyUserRepository.java
@@ -1,11 +1,13 @@
 package server.use_cases;
 
+import server.entities.User;
 import server.use_cases.repo_abstracts.IUserRepository;
 import server.use_cases.repo_abstracts.UserNotFoundException;
 import server.use_cases.repo_abstracts.UserRepositoryUserAccountFetcherResponse;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,14 +16,16 @@ import java.util.Map;
 class DummyUserRepositoryEntity{
     private final String firstName;
     private final String lastName;
-    private final String homeAddress;
+    private final String currentAddress;
+    private final String currentCity;
     private final String passwordHash;
     private final String email;
 
-    public DummyUserRepositoryEntity(String firstName, String lastName, String homeAddress, String passwordHash, String email) {
+    public DummyUserRepositoryEntity(String firstName, String lastName, String currentAddress, String currentCity, String passwordHash, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.homeAddress = homeAddress;
+        this.currentAddress = currentAddress;
+        this.currentCity = currentCity;
         this.passwordHash = passwordHash;
         this.email = email;
     }
@@ -34,8 +38,12 @@ class DummyUserRepositoryEntity{
         return lastName;
     }
 
-    public String getHomeAddress() {
-        return homeAddress;
+    public String getCurrentAddress() {
+        return currentAddress;
+    }
+
+    public String getCurrentCity() {
+        return currentCity;
     }
 
     public String getPasswordHash() {
@@ -60,10 +68,10 @@ public class DummyUserRepository implements IUserRepository {
     }
 
     @Override
-    public int createUser(String firstName, String lastName, String homeAddress, String passwordHash, String email) {
+    public int createUser(String firstName, String lastName, String currentAddress, String currentCity, String passwordHash, String email) {
         currentMaxId++;
         int id = currentMaxId;
-        users.add(new DummyUserRepositoryEntity(firstName, lastName, homeAddress, passwordHash, email));
+        users.add(new DummyUserRepositoryEntity(firstName, lastName, currentAddress, currentCity, passwordHash, email));
 
         return id;
     }
@@ -73,7 +81,7 @@ public class DummyUserRepository implements IUserRepository {
         if (userId >= 0 && userId <= currentMaxId){
             DummyUserRepositoryEntity user = users.get(userId);
             return new UserRepositoryUserAccountFetcherResponse(user.getFirstName(), user.getLastName(),
-                    user.getHomeAddress(), user.getEmail());
+                    user.getCurrentAddress(), user.getCurrentCity(), user.getEmail());
         }else{
             throw new UserNotFoundException("User with ID: " + userId + " not found.");
         }
@@ -89,4 +97,20 @@ public class DummyUserRepository implements IUserRepository {
 
         return false;
     }
+
+    @Override
+    public List<User> fetchAllUsers() {
+        List<DummyUserRepositoryEntity> dbUsers = this.users;
+        List<User> users = new ArrayList<>();
+
+        // TODO factory pattern
+        for (DummyUserRepositoryEntity dbUser : dbUsers) {
+            User user = new User(dbUser.getFirstName(), dbUser.getLastName(), dbUser.getCurrentAddress(),
+                    dbUser.getCurrentCity(), dbUser.getPasswordHash(), dbUser.getEmail()) {};
+            users.add(user);
+        }
+
+        return users;
+    }
+
 }
