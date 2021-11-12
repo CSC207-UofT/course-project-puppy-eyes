b35b279fd44d0d8eda05ee25ea8d47eb1aa67864
Kenneth Tran <kennethtran77@gmail.com>
2021-11-12 06:44:23 -0500

    Update entities in repos

--- src/main/java/server/use_cases/repo_abstracts/IPetRepository.java
+++ src/main/java/server/use_cases/repo_abstracts/IPetRepository.java
@@ -5,16 +5,16 @@ public interface IPetRepository {
     /**
      * Create a new Pet and save it in the repository.
      *
+     * @param userId The user id of this pet's owner
      * @param name The pet's name.
      * @param age The Pet's age.
      * @return New pet's id.
      */
-    public int createPet(String name, int age);
-
+    public int createPet(int userId, String name, int age);
 
     // TODO: Complete following methods (return, args,...)
     public void fetchPetProfile();
-    public void matchPet();
+
     public void editPet();
 
 }
