4f5994ef011418d026651ebb2a93d411791a18a9
Kenneth Tran <kennethtran77@gmail.com>
2021-11-12 05:46:57 -0500

    Implement Relation database and repository

---
+++ src/main/java/server/drivers/repository/RelationRepository.java
@@ -0,0 +1,59 @@
+package server.drivers.repository;
+
+import org.springframework.stereotype.Repository;
+import server.drivers.dbEntities.RelationDatabaseEntity;
+import server.use_cases.repo_abstracts.IRelationRepository;
+
+import java.util.Optional;
+
+@Repository
+public class RelationRepository implements IRelationRepository {
+
+    private final JpaRelationRepository repository;
+
+    public RelationRepository(JpaRelationRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Add a relationship between two ids
+     * @param fromId
+     * @param toId
+     * @param relationType
+     */
+    @Override
+    public void addRelation(int fromId, int toId, String relationType) {
+        RelationDatabaseEntity relation = new RelationDatabaseEntity(fromId, toId, relationType);
+        this.repository.save(relation);
+    }
+
+    /**
+     * Remove a relationship between two ids
+     * @param fromId
+     * @param toId
+     * @param relationType
+     */
+    @Override
+    public void removeRelation(int fromId, int toId, String relationType) {
+        Optional<RelationDatabaseEntity> relation = Optional.ofNullable(this.repository.
+                findRelationDatabaseEntityByFromIdAndToId(fromId, toId));
+
+        if (!relation.isPresent()) {
+            return;
+        }
+
+        this.repository.delete(relation.get());
+    }
+
+    /**
+     * Return whether fromId has a relation to toId with type relationType
+     * @param fromId
+     * @param toId
+     * @param relationType
+     * @return whether fromId has a relation to toId with type relationType
+     */
+    @Override
+    public boolean hasRelation(int fromId, int toId, String relationType) {
+        return this.repository.existsByFromIdAndToIdAndRelationType(fromId, toId, relationType);
+    }
+}
