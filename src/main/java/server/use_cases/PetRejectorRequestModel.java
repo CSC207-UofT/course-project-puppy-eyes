97623de53753fda457821009b9d3dc161beaafb3
Kenneth Tran <kennethtran77@gmail.com>
2021-11-13 17:38:41 -0500

    Implement PetRejector use case

---
+++ src/main/java/server/use_cases/PetRejectorRequestModel.java
@@ -0,0 +1,21 @@
+package server.use_cases;
+
+public class PetRejectorRequestModel {
+
+    private int pet1Id;
+    private int pet2Id;
+
+    public PetRejectorRequestModel(int pet1Id, int pet2Id) {
+        this.pet1Id = pet1Id;
+        this.pet2Id = pet2Id;
+    }
+
+    public int getFirstPetId() {
+        return this.pet1Id;
+    }
+
+    public int getSecondPetId() {
+        return this.pet2Id;
+    }
+
+}
