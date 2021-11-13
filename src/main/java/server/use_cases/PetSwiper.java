ec9135e2d76bcb5d386b1ef2ccb1489cdb6ae401
Kenneth Tran <kennethtran77@gmail.com>
2021-11-13 17:39:19 -0500

    Merge Matching into PetSwiper use case

--- src/main/java/server/use_cases/PetSwiper.java
+++ src/main/java/server/use_cases/PetSwiper.java
@@ -17,23 +17,26 @@ public class PetSwiper implements PetSwiperInputBoundary {
     public PetSwiperResponseModel swipe(PetSwiperRequestModel request) {
         PetSwiperResponseModel response = new PetSwiperResponseModel(true);
 
-        final String relationType = "SWIPE";
+        final String swipeRelationType = "SWIPE";
+        final String matchRelationType = "MATCH";
 
         // If the first pet already swiped on the second pet, do nothing.
-        if (this.relationRepository.hasRelation(request.getFirstPetId(), request.getSecondPetId(), relationType)) {
+        if (this.relationRepository.hasRelation(request.getFirstPetId(), request.getSecondPetId(), swipeRelationType)) {
             return response;
         }
 
         // If the second pet already swiped on the first pet,
         // remove the first pet from the second pet's swiped list and match both of them
-        if (this.relationRepository.hasRelation(request.getSecondPetId(), request.getFirstPetId(), relationType)) {
-            this.relationRepository.removeRelation(request.getSecondPetId(), request.getFirstPetId(), relationType);
+        if (this.relationRepository.hasRelation(request.getSecondPetId(), request.getFirstPetId(), swipeRelationType)) {
+            this.relationRepository.removeRelation(request.getSecondPetId(), request.getFirstPetId(), swipeRelationType);
+            this.relationRepository.addRelation(request.getFirstPetId(), request.getSecondPetId(), matchRelationType);
+            this.relationRepository.addRelation(request.getSecondPetId(), request.getFirstPetId(), matchRelationType);
             return response;
         }
 
         // If neither pet swiped on each other,
         // add the second pet to the first pet's swiped list
-        this.relationRepository.addRelation(request.getFirstPetId(), request.getSecondPetId(), relationType);
+        this.relationRepository.addRelation(request.getFirstPetId(), request.getSecondPetId(), swipeRelationType);
         return response;
     }
 }
