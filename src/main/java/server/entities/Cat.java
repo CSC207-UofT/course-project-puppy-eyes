8205394226e4f3e7eaf897b2a9bc6776f9d5458d
Kenneth Tran <kennethtran77@gmail.com>
2021-11-20 10:12:54 -0500

    Begin implementing subclasses of Pet

---
+++ src/main/java/server/entities/Cat.java
@@ -0,0 +1,9 @@
+package server.entities;
+
+public class Cat extends Pet {
+
+    public Cat(int userId, String name, int age, String breed, String biography) {
+        super(userId, name, age, breed, biography);
+    }
+
+}
