cc097aca2ab0f22e5615b49c1886b719ef42d7b9
Kenneth Tran <kennethtran77@gmail.com>
2021-11-26 01:27:48 -0500

    Add util class

---
+++ src/main/java/server/use_cases/Util.java
@@ -0,0 +1,18 @@
+package server.use_cases;
+
+import java.util.regex.Pattern;
+
+/**
+ * A util class containing static methods for common operations.
+ */
+public final class Util {
+
+    private Util() { }
+
+    private final static Pattern intPattern = Pattern.compile("-?[0-9]+");
+
+    public static boolean isInteger(String string) {
+        return intPattern.matcher(string).matches();
+    }
+
+}
