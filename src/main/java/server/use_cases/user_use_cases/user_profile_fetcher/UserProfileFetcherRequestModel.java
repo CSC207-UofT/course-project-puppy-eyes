8b447ce7790b0bbadd7c3fda3ded734008ad3df9
Kenneth Tran <kennethtran77@gmail.com>
2021-12-05 23:40:58 -0500

    Remove unused code

--- src/main/java/server/use_cases/user_use_cases/user_profile_fetcher/UserProfileFetcherRequestModel.java
+++ src/main/java/server/use_cases/user_use_cases/user_profile_fetcher/UserProfileFetcherRequestModel.java
@@ -8,23 +8,8 @@ import server.use_cases.AuthRequestModel;
  */
 public class UserProfileFetcherRequestModel extends AuthRequestModel {
 
-    private boolean hasMutualPets;
-
     public UserProfileFetcherRequestModel(String headerUserId, String userId) {
         super(headerUserId, userId);
-    }
-
-    public boolean hasMutualPets() {
-        return hasMutualPets;
-    }
-
-    public void setHasMutualPets(boolean hasMutualPets) {
-        this.hasMutualPets = hasMutualPets;
-    }
-
-    @Override
-    public boolean isRequestAuthorized() {
-        return super.isRequestAuthorized() || hasMutualPets;
     }
 
 }
