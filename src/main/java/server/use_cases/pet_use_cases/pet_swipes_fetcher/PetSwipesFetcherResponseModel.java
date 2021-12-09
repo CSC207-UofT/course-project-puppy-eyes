490368bf60dc89190a79826d7fea5faa97c2ca7a
Kenneth Tran <kennethtran77@gmail.com>
2021-12-08 20:38:18 -0500

    Fix typo in PetSwipesFetcher and PetSwipesFetcherResponseModel

--- src/main/java/server/use_cases/pet_use_cases/pet_swipes_fetcher/PetSwipesFetcherResponseModel.java
+++ src/main/java/server/use_cases/pet_use_cases/pet_swipes_fetcher/PetSwipesFetcherResponseModel.java
@@ -1,12 +1,14 @@
 package server.use_cases.pet_use_cases.pet_swipes_fetcher;
 
+import server.use_cases.ResponseData;
+
 import java.util.List;
 
 /**
  * An object defining the request type for
  * PetSwipesFetcherInputBoundary.fetchPetSwipes
  */
-public class PetSwipesFetcherResponseModel {
+public class PetSwipesFetcherResponseModel extends ResponseData {
 
     private final List<String> petIds;
 
