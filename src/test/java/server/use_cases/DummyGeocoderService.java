91bb27ff9c19a790643cf5992303cea70f12e4a9
Kenneth Tran <kennethtran77@gmail.com>
2021-12-08 19:36:39 -0500

    Add test geocode change in UserAccountEditor

--- src/test/java/server/use_cases/DummyGeocoderService.java
+++ src/test/java/server/use_cases/DummyGeocoderService.java
@@ -3,18 +3,25 @@ package server.use_cases;
 import server.drivers.IGeocoderService;
 import server.drivers.LatLng;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DummyGeocoderService implements IGeocoderService {
 
     /**
-     * A dummy geocoder service that returns the lat-lng of Toronto regardless of query.
+     * A dummy geocoder service that returns the lat-lng of some select cities.
      * @param query
-     * @return a List<LatLng> of one element
+     * @return a List<LatLng>
      */
     @Override
     public List<LatLng> getLatLng(String query) {
-        return Arrays.asList(new LatLng(43.6532, 79.3832));
+        if (query.toLowerCase().contains("toronto")) {
+            return Arrays.asList(new LatLng(43.6532, 79.3832));
+        } else if (query.toLowerCase().contains("new york")) {
+            return Arrays.asList(new LatLng(40.7128, 74.0060));
+        }
+
+        return new ArrayList<>();
     }
 }
